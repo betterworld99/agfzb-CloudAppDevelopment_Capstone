01a941c57b4137e1b50e2ec0fba1778fcd39c136
betterworld99 <106548202+betterworld99@users.noreply.github.com>
2024-01-12 22:53:08 +0330

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -9,39 +9,75 @@ from django.contrib import messages
 from datetime import datetime
 import logging
 import json
+from django.contrib.auth.forms import UserCreationForm
+# from .forms import LoginForm,SignUpForm
+from django.contrib.auth.decorators import login_required
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
 
-# Create your views here.
 
 
-# Create an `about` view to render a static about page
-# def about(request):
-# ...
+
+
+
+
+def about_view(request):
+    return render(request, 'djangoapp/about.html')
+
+
+
 
 
 # Create a `contact` view to return a static contact page
-#def contact(request):
+def contact(request):
+    return render(request, 'djangoapp/contact.html')
 
-# Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
 
-# Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
 
-# Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+@login_required
+def profile_view(request):
+    return render(request, 'djangoapp/profile.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login successful.')
+            return redirect('djangoapp:profile')  # Redirect to the profile page upon successful login
+        else:
+            messages.error(request, 'Invalid login credentials.')
+
+    return render(request, 'djangoapp/login.html')
+
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}. You can now log in.')
+            return redirect('djangoapp:login')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'djangoapp/register.html', {'form': form})
+
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
     context = {}
     if request.method == "GET":
         return render(request, 'djangoapp/index.html', context)
+        
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
